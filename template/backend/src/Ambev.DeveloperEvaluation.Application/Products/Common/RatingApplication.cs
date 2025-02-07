d4027a7708e0feb736865ed1808cbc43fd790cb9
Daniel Silva <dnlwillian@gmail.com>
2025-02-07 17:56:44 -0300

    FEAT - PRODUCT - Common application product objects

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Products/Common/RatingApplication.cs
@@ -0,0 +1,8 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Products.Common
+{
+    public class RatingApplication
+    {
+        public decimal Rate { get; set; }
+        public int Count { get; set; }
+    }
+}
