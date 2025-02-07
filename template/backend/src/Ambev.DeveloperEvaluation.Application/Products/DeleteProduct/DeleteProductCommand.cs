6a221be3176ed1746961901e064030c07478ef02
Daniel Silva <dnlwillian@gmail.com>
2025-02-07 18:01:19 -0300

    FEAT - PRODUCT - Delete product commands

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Products/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,13 @@
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Products.DeleteProduct;
+
+public record DeleteProductCommand : IRequest<DeleteProductResponse>
+{
+    public int Id { get; }
+
+    public DeleteProductCommand(int id)
+    {
+        Id = id;
+    }
+}
