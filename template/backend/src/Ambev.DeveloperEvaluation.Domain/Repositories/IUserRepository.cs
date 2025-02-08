027327f0d73551d15596c0d74186a548854cc916
Daniel Silva <dnlwillian@gmail.com>
2025-02-07 22:08:19 -0300

    FEAT - USER - User repository list and update

--- template/backend/src/Ambev.DeveloperEvaluation.Domain/Repositories/IUserRepository.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Repositories/IUserRepository.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+using Ambev.DeveloperEvaluation.Domain.Common;
 using Ambev.DeveloperEvaluation.Domain.Entities;
 
 namespace Ambev.DeveloperEvaluation.Domain.Repositories;
@@ -16,6 +18,14 @@ public interface IUserRepository
     Task<User> CreateAsync(User user, CancellationToken cancellationToken = default);
 
     /// <summary>
+    /// Updates a new user in the repository
+    /// </summary>
+    /// <param name="user">The user to create</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The updated user</returns>
+    Task<User> UpdateAsync(User user, CancellationToken cancellationToken = default);
+
+    /// <summary>
     /// Retrieves a user by their unique identifier
     /// </summary>
     /// <param name="id">The unique identifier of the user</param>
@@ -30,6 +40,22 @@ public interface IUserRepository
     /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>The user if found, null otherwise</returns>
     Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves a user by predicate
+    /// </summary>
+    /// <param name="email">The email address to search for</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The user if found, null otherwise</returns>
+    Task<User?> GetByAsync(Expression<Func<User, bool>> predicate, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves all users with pagination and sorting.
+    /// </summary>
+    /// <param name="request">The pagination and sorting request parameters.</param>
+    /// <param name="cancellationToken">The cancellation token to cancel the operation.</param>
+    /// <returns>A paginated response containing the list of users.</returns>
+    Task<ApiQueryResponseDomain<User>> GetAllUsersAsync(ApiQueryRequestDomain request, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Deletes a user from the repository
--- template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/UserRepository.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/UserRepository.cs
@@ -1,4 +1,7 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+﻿using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
+using Ambev.DeveloperEvaluation.Domain.Common;
+using Ambev.DeveloperEvaluation.Domain.Entities;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
 using Microsoft.EntityFrameworkCore;
 
@@ -34,6 +37,19 @@ public class UserRepository : IUserRepository
     }
 
     /// <summary>
+    /// Updates a new user in the database
+    /// </summary>
+    /// <param name="user">The user to create</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The updated user</returns>
+    public async Task<User> UpdateAsync(User user, CancellationToken cancellationToken = default)
+    {
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync(cancellationToken);
+        return user;
+    }
+
+    /// <summary>
     /// Retrieves a user by their unique identifier
     /// </summary>
     /// <param name="id">The unique identifier of the user</param>
@@ -41,7 +57,7 @@ public class UserRepository : IUserRepository
     /// <returns>The user if found, null otherwise</returns>
     public async Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
     {
-        return await _context.Users.FirstOrDefaultAsync(o=> o.Id == id, cancellationToken);
+        return await _context.Users.FirstOrDefaultAsync(o => o.Id == id, cancellationToken);
     }
 
     /// <summary>
@@ -54,6 +70,47 @@ public class UserRepository : IUserRepository
     {
         return await _context.Users
             .FirstOrDefaultAsync(u => u.Email == email, cancellationToken);
+    }
+
+    /// <summary>
+    /// Retrieves a user by predicate
+    /// </summary>
+    /// <param name="email">The email address to search for</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The user if found, null otherwise</returns>
+    public async Task<User?> GetByAsync(Expression<Func<User, bool>> predicate, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users
+            .FirstOrDefaultAsync(predicate);
+    }
+
+    /// <summary>
+    /// Retrieves all users in the database with pagination and sorting.
+    /// </summary>
+    /// <param name="request">The pagination and sorting request parameters.</param>
+    /// <param name="cancellationToken">The cancellation token to cancel the operation.</param>
+    /// <returns>A paginated response containing the list of users.</returns>
+    public async Task<ApiQueryResponseDomain<User>> GetAllUsersAsync(ApiQueryRequestDomain request, CancellationToken cancellationToken = default)
+    {
+        var query = _context.Users.AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(request.Order))
+            query = query.OrderBy(request.Order.Trim());
+
+        int totalItems = await query.CountAsync(cancellationToken);
+
+        var data = await query
+            .Skip((request.Page - 1) * request.Size)
+            .Take(request.Size)
+            .ToListAsync(cancellationToken);
+
+        return new ApiQueryResponseDomain<User>
+        {
+            Data = data,
+            TotalItems = totalItems,
+            CurrentPage = request.Page,
+            TotalPages = (int)Math.Ceiling(totalItems / (double)request.Size)
+        };
     }
 
     /// <summary>
