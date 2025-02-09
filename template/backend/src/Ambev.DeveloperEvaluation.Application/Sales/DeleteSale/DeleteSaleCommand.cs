2b840eca30f9034f9c8468ba1778178cffde7096
Daniel Silva <dnlwillian@gmail.com>
2025-02-09 12:00:04 -0300

    FEAT - SALES - Delete sale commands

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleCommand.cs
@@ -0,0 +1,13 @@
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+
+public record DeleteSaleCommand : IRequest<DeleteSaleResponse>
+{
+    public int Id { get; }
+
+    public DeleteSaleCommand(int id)
+    {
+        Id = id;
+    }
+}
