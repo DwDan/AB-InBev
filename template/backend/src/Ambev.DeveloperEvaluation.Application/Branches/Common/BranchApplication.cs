9fe000728a16d9a6cd9c0d1fab6c4dce6c859153
Daniel Silva <dnlwillian@gmail.com>
2025-02-09 10:31:11 -0300

    FEAT - BRANCHES - Common files branches

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Branches/Common/BranchApplication.cs
@@ -0,0 +1,8 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Branches.Common
+{
+    public class BranchApplication
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Branches/Common/BranchPresentation.cs
@@ -0,0 +1,8 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Branches.Common
+{
+    public class BranchPresentation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
