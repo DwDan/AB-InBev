7e31f8e7f0882c8052162299aebe2adf9f56facb
Daniel Silva <dnlwillian@gmail.com>
2025-02-09 12:00:17 -0300

    FEAT - SALES - Get sale commands

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/GetSale/GetSaleResponse.cs
@@ -0,0 +1,5 @@
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.Common;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale;
+
+public class GetSaleResponse : SalePresentation { }
