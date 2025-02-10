1df5d95dba4409574a1a86a95320559c87416992
Daniel Silva <dnlwillian@gmail.com>
2025-02-10 19:14:13 -0300

    FEAT - GENERAL - Home component

---
+++ template/frontend/ambev.developer-evaluation/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../core/shared/modules/material.module';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule, MaterialModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent { }
